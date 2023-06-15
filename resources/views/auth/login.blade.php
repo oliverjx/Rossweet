fc404b9743799015d5f58bd8a70c438818c3090d
oliverjx <115738356+oliverjx@users.noreply.github.com>
2023-06-15 18:12:34 -0400

    imagen de logo en formato .ico

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('') }}</div>
-
-                <div class="card-body">
+    
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -65,9 +61,9 @@
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
+
+
+
