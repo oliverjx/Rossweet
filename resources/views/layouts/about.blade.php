d69345949fa16a6798aac6ad3d7e0f954889e747
oliverjx <115738356+oliverjx@users.noreply.github.com>
2023-06-16 19:37:51 -0400

    agregue width a la imagen de about

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -22,7 +22,7 @@ primer año.
                     </p>
                 </div>
                 <div class="col-md-4">
-                    <img src="{{ asset('img/sobre-nosotros.jpg') }}" alt="About Hero">
+                    <img width="70%"  src="{{ asset('img/sobre-nosotros.jpg') }}" alt="About Hero">
                 </div>
             </div>
         </div>
