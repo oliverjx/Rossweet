038c862adfa23102b1a1a784ac41b410eb413590
oliverjx <115738356+oliverjx@users.noreply.github.com>
2023-06-18 08:52:07 -0400

    template para table

---
+++ resources/views/layouts/table.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+@section('relleno')
+    <!-- Open Content -->
+    <section class="bg-light">
+        <div class="container pb-5">
+            <div class="row">
+                <!-- col end -->
+                <div class="col-lg-7 mt-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1 class="h2">Agrega Categorias:</h1>
+                       
+                            {{-- aqui coloca el contenido de la tabla --}}
+
+
+
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Close Content -->
+@endsection
