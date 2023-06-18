9677919b7ddd3c8f00fde6a6c65ba8cd2ee9b8c6
oliverjx <115738356+oliverjx@users.noreply.github.com>
2023-06-18 12:36:25 -0400

    movia del link de style para el modal

--- resources/views/layouts/prueba.blade.php
+++ resources/views/layouts/prueba.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.main')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 @section('relleno')
     <!-- Open Content -->
     <section class="bg-light">
